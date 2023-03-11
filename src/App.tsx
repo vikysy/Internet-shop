a98b8d93489ffe5bbbc4fb60eb607f3cab8122b2
Viktoriya Eshukova <viktoria.eshukova@yandex.ru>
2023-03-11 12:27:28 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 import Footer from './components/footer/Footer';
 import { Layout } from './components/Layout';
